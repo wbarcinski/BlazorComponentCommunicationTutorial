6f6b70bbe28fff39ff0143de0d603eee509118ef
WITOLD BARCINSKI <wbarcinski@hotmail.com>
2022-08-23 09:42:38 -0500

    Add project files.

---
+++ BlazorComponentCommunicationTutorial/Services/INotificationService.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorComponentCommunicationTutorial.Services
+{
+    public interface INotificationService
+    {
+        event Action Onchange;
+        int Count { get; set; }
+        void IncrementCounter();
+        void ReserCounter();
+
+    }
+}
---
+++ BlazorComponentCommunicationTutorial/Services/NotificationService.cs
@@ -0,0 +1,21 @@
+﻿namespace BlazorComponentCommunicationTutorial.Services
+{
+    public class NotificationService : INotificationService
+    {
+        public int Count { get; set; } = 0;
+
+        public event Action Onchange;
+
+        public void IncrementCounter()
+        {
+            Count++;
+            Onchange?.Invoke();
+        }
+
+        public void ReserCounter()
+        {
+            Count=0;
+            Onchange?.Invoke();
+        }
+    }
+}
